1a1e59cfefbbff0cbd943b56f038e50313268022
Khushal Bhardwaj <khushalbhardwaj0111@gmail.com>
2023-04-29 03:55:26 +0530

    basic event show done

--- src/components/Calendar/TimeBlocks/TimeBlockRow.tsx
+++ src/components/Calendar/TimeBlocks/TimeBlockRow.tsx
@@ -7,13 +7,12 @@ export const TimeBlockRow: React.FC<{
   idxHr: number;
   weekDates: string[];
 }> = ({ idxHr, current, weekDates }) => {
-  const { data: events, isLoading } = api.event.getAll.useQuery();
+  const { data: events, isLoading } = api.event.getAllWeek.useQuery({
+    start: weekDates[0]!,
+    end: weekDates[weekDates.length - 1]!,
+  });
 
-  useEffect(() => {
-    console.log(events);
-  }, [isLoading]);
-
-  if (isLoading) return <div className="h-[1520px]">Loading...</div>;
+  if (isLoading || !events) return <div className="h-[1520px]">Loading...</div>;
 
   return (
     <div className="flex h-20 flex-[calc(1/19)]">
@@ -24,6 +23,7 @@ export const TimeBlockRow: React.FC<{
           idxDay={idxDay}
           current={current}
           idxHr={idxHr}
+          events={events}
         />
       ))}
     </div>
